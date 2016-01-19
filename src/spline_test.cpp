1bb194e3c7940fe37bb0fba4b494f8bdca082523
zahra boroujeni <zahra.boroujeni@gmail.com>
2016-01-19 15:25:21 +0100

    3 spline

--- src/spline_test.cpp
+++ src/spline_test.cpp
@@ -33,62 +33,80 @@ using ecl::CubicSpline;
 /*****************************************************************************
 ** Main program
 *****************************************************************************/
+nav_msgs::Path subsample(CubicSpline cubic,float start_x, float end_x)
+{
+    std::vector<geometry_msgs::PoseStamped> plan;
+    nav_msgs::Path gui_path;
+   geometry_msgs::PoseStamped new_goal;
+    //ros::Rate loop_rate(1);
+   for (int i=0; i<200; i++){
 
+      //tf::Quaternion goal_quat = tf::createQuaternionFromRPY(0,0,1);
+
+
+      new_goal.pose.position.x = i*(end_x-start_x)/200+start_x;
+      new_goal.pose.position.y = cubic(i*(end_x-start_x)/200+start_x);
+       new_goal.pose.orientation.x = 0;
+       new_goal.pose.orientation.y =0;
+       new_goal.pose.orientation.z = 0;
+       new_goal.pose.orientation.w =1;
+       new_goal.header.stamp = ros::Time::now();
+       new_goal.header.seq++;
+      plan.push_back(new_goal);
+
+   }
+   gui_path.poses.resize(plan.size());
+
+    if(!plan.empty()){
+          gui_path.header.frame_id = "map";
+          gui_path.header.stamp = plan[0].header.stamp;
+    }
+
+    for(unsigned int i=0; i < plan.size(); i++){
+          gui_path.poses[i] = plan[i];
+    }
+    return gui_path;
+
+}
 int main(int argc, char **argv){
+
 
     ros::init(argc,argv,"test_spline");
 
-    Array<double> x_set(4);
-    Array<double> y_set(4);
-    x_set << 0.688792, 1.15454, 1.67894,0.8;
-    y_set << -0.75, -1.2, -1.30,1.4;
+    Array<double> x_set(10);
+    Array<double> y_set(10);
+    x_set << 0.688792, 1.15454, 1.67894,2.1,2.7,3.1,3.6,4,5,6;
+    y_set << -0.75, -1.2, -0.50,-1.4,-1,0,0.1,1.3,0.3,1.0;
+    double ydot_0 = -0.5/x_set[0];
+        double ydot_f = 1.04/3.72;
 
-    CubicSpline cubic = CubicSpline::Natural(x_set, y_set);
-    const CubicPolynomial &p1 = cubic.polynomials()[0];
-    const CubicPolynomial &p2 = cubic.polynomials()[1];
-    const CubicPolynomial &p3 = cubic.polynomials()[2];
-    std::cout << cubic << std::endl;
-    std::cout << "Value         : " << cubic(0.8) << std::endl;
-    std::cout << "Coffiecinets         : " << p1.coefficients()<< std::endl;
-       ros::NodeHandle n;
-       //ros::Publisher chatter_pub = n.advertise<std_msgs::Float64 >("/drc_vehicle_xp900/gas_pedal/cmd",1000);
+    CubicSpline cubic1 = CubicSpline::Natural(x_set, y_set);
+    CubicSpline cubic2 = CubicSpline::DerivativeHeuristic(x_set, y_set, ydot_0, ydot_f);
+    CubicSpline cubic3 = CubicSpline::ContinuousDerivatives(x_set, y_set, ydot_0, ydot_f);
 
-       ros::Publisher path_pub = n.advertise<nav_msgs::Path>("/path", 1000);
-       std::vector<geometry_msgs::PoseStamped> plan;
-       nav_msgs::Path gui_path;
+//    const CubicPolynomial &p1 = cubic1.polynomials()[0];
+//    const CubicPolynomial &p2 = cubic1.polynomials()[1];
+//    const CubicPolynomial &p3 = cubic1.polynomials()[2];
+//    std::cout << cubic1 << std::endl;
+//    std::cout << "Value         : " << cubic1(0.8) << std::endl;
+//    std::cout << "Coffiecinets         : " << p1.coefficients()<< std::endl;
 
-       //ros::Rate loop_rate(1);
-      for (int i=0; i<200; i++){
-        geometry_msgs::PoseStamped new_goal;
-         //tf::Quaternion goal_quat = tf::createQuaternionFromRPY(0,0,1);
-
-
-         new_goal.pose.position.x = i*(1.67894-0.688792)/200+0.688792;
-         new_goal.pose.position.y = cubic(i*(1.67894-0.688792)/200+0.688792);
-
-
-          new_goal.pose.orientation.x = 0;
-          new_goal.pose.orientation.y =0;
-          new_goal.pose.orientation.z = 0;
-          new_goal.pose.orientation.w =1;
-
-         plan.push_back(new_goal);
-      }
-      gui_path.poses.resize(plan.size());
-
-       if(!plan.empty()){
-             gui_path.header.frame_id = "map";
-             gui_path.header.stamp = plan[0].header.stamp;
-       }
-
-       for(unsigned int i=0; i < plan.size(); i++){
-             gui_path.poses[i] = plan[i];
-       }
-
+    ros::NodeHandle n;
+    ros::Publisher path_pub1 = n.advertise<nav_msgs::Path>("/path_Natural", 1000);
+    ros::Publisher path_pub2 = n.advertise<nav_msgs::Path>("/path2_DerivativeHeuristic", 1000);
+     ros::Publisher path_pub3 = n.advertise<nav_msgs::Path>("/path3_ContinuousDerivatives", 1000);
+    nav_msgs::Path gui_path1;
+     nav_msgs::Path gui_path2;
+     nav_msgs::Path gui_path3;
 
        while (ros::ok())
        {
-           path_pub.publish(gui_path);
+           gui_path1=subsample(cubic1,0.688792,6);
+           path_pub1.publish(gui_path1);
+           gui_path2=subsample(cubic2,0.688792,6);
+           path_pub2.publish(gui_path2);
+           gui_path3=subsample(cubic3,0.688792,6);
+           path_pub3.publish(gui_path3);
            ros::spinOnce();
        }
        return 0;
